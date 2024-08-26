5066a4a48880f89f6fd2245e02719c2ff27ea085
Kastan Day <kastanvday@gmail.com>
2024-08-26 23:29:20 +0000

    Fully working with kubernetes. Now I'm pickeling the minio uploads

--- Dockerfile
+++ Dockerfile
@@ -33,7 +33,14 @@ RUN pip install -e pathway_search_standalone/rxn_cluster_token_prompt
 
 COPY . /app
 
-# Compute expensive, able to be GPU-accelerated. It's a rigerous test of full functinoality.
+# Remove the frequently changing files if they were copied
+RUN rm -f /app/test_docker_build.py /app/entrypoint.py
+
+# add frequenty changing files here (AND IN THE .dockerignore) to preserve build cache
+COPY --chown=root:root test_docker_build.py /app/
+COPY --chown=root:root entrypoint.py /app/
+
+# A rigerous test of full functinoality. (OPTIONAL, Compute expensive, able to be GPU-accelerated.)
 # RUN python test_docker_build.py
 
 # default command
--- entrypoint.py
+++ entrypoint.py
@@ -5,12 +5,17 @@ It takes input and returns output via Minio.
 """
 
 import argparse
+import pickle
 import io
 import sys
 import joblib
 import six
 import json
 import time
+# Keep these lines before any other imports (need to override defaults)
+sys.modules['sklearn.externals.joblib'] = joblib
+sys.modules['sklearn.externals.six'] = six
+
 from sfscore import SFScore
 from pathway_search_standalone.scripts.search_utils import hybridSearch, build_graph_from_async
 import networkx as nx
@@ -31,9 +36,6 @@ def score_from_smi(smiles):
     return sfscore
 
 def get_chemoenzy_path_async(smiles, max_depth=10, chem_topk=10, max_num_templates=250, max_branching=15, time_lim=180):
-    sys.modules['sklearn.externals.joblib'] = joblib
-    sys.modules['sklearn.externals.six'] = six
-
     hybrid_search = hybridSearch()
     explored_rxns, explored_nodes, start_node = hybrid_search.get_chemoenzy_path_async(
         smiles, max_depth=max_depth, chem_topk=chem_topk, max_num_templates=max_num_templates,
@@ -53,7 +55,6 @@ def build_graph_from_async_wrapper(explored_rxns, explored_nodes, start_node):
     return graph_data
 
 def download_json_from_minio(bucket: str ='', path: str=''):
-    # Initialize the Minio client
     client = Minio(
         os.environ['MINIO_URL'],  
         access_key=os.environ['MINIO_ACCESS_KEY'],
@@ -73,7 +74,6 @@ def download_json_from_minio(bucket: str ='', path: str=''):
         raise
 
 def upload_json_to_minio(data, bucket: str ='', path: str=''):
-    # Initialize the Minio client
     client = Minio(
         os.environ['MINIO_URL'],  
         access_key=os.environ['MINIO_ACCESS_KEY'],
@@ -82,10 +82,15 @@ def upload_json_to_minio(data, bucket: str ='', path: str=''):
     )
 
     try:
-        # Convert data to JSON
-        json_data = json.dumps(data).encode('utf-8')
-        # Upload the JSON data to Minio
-        client.put_object(bucket, path, data=io.BytesIO(json_data), length=len(json_data), content_type='application/json')
+        # # Convert data to JSON
+        # json_data = json.dumps(data).encode('utf-8')
+        # # Upload the JSON data to Minio
+        # client.put_object(bucket, path, data=io.BytesIO(json_data), length=len(json_data), content_type='application/json')
+        
+        # Serialize data to binary using pickle
+        binary_data = pickle.dumps(data)
+        # Upload the binary data to Minio
+        client.put_object(bucket, path, data=io.BytesIO(binary_data), length=len(binary_data), content_type='application/octet-stream')
         print(f"Successfully uploaded results to {bucket}/{path}")
     except S3Error as e:
         print("Error occurred while uploading.", e)
@@ -93,51 +98,55 @@ def upload_json_to_minio(data, bucket: str ='', path: str=''):
 
 def main():
     parser = argparse.ArgumentParser(description="Run various chemical analysis functions.")
-    # parser.add_argument('function', choices=['score_from_smi', 'get_chemoenzy_path_async', 'build_graph_from_async'],
-    #                     help='Function to run')
     parser.add_argument('--job_id', required=True, help='Minio path to the JSON file containing input parameters')
     args = parser.parse_args()
 
-    # Download the JSON file from Minio
-    # TODO: Get env vars for Minio
-    # params = download_json_from_minio(bucket='aceretro', path=f"/{args.job_id}/in/input.json")
-
-    params = {
-        'smiles': 'O=C(COP(=O)(O)O)[C@H](O)[C@H](O)CO',
-    }
+    params = None
+    try: 
+        # Download the input JSON file from Minio
+        params = download_json_from_minio(bucket='aceretro', path=f"/{args.job_id}/in/input.json")
+    except Exception as e: 
+        print(f"xxx Failed to download input from Minio with error: {e}")
+        print("WARNING: FALLING BACK TO PLACEHOLDER DEFAULT SMILES STRING... `O=C(COP(=O)(O)O)[C@H](O)[C@H](O)CO`")
+        params = {
+            'smiles': 'O=C(COP(=O)(O)O)[C@H](O)[C@H](O)CO',
+        }
 
     results = {}
     start_time = time.monotonic()
     if 'smiles' not in params:
-        parser.error("❌ Error: The input JSON file must contain 'smiles' for score_from_smi")
+        parser.error("xxx Error: The input JSON file must contain 'smiles' for score_from_smi")
     else: 
         results['sfscore'] = score_from_smi(params['smiles'])
-        print(f"[1/3] Completed score_from_smi(). ⏰ Runtime: {(time.monotonic() - start_time):.2f} seconds")
+        print(f"[1/3] Completed score_from_smi(). Runtime: {(time.monotonic() - start_time):.2f} seconds")
     
     start_time = time.monotonic()
     if 'smiles' not in params:
-        parser.error("❌ Error: The input JSON file must contain 'smiles' for get_chemoenzy_path_async")
+        parser.error("xxx Error: The input JSON file must contain 'smiles' for get_chemoenzy_path_async")
     else: 
         explored_rxns, explored_nodes, start_node = get_chemoenzy_path_async(
             params['smiles'], params.get('max_depth', 10), params.get('chem_topk', 10), 
             params.get('max_num_templates', 250), params.get('max_branching', 15), 
-            params.get('time_lim', 180)
+            params.get('time_lim', 20)
         )
         results['explored_rxns'] = explored_rxns
         results['explored_nodes'] = explored_nodes
         results['start_node'] = start_node
-        print(f"[2/3] Completed get_chemoenzy_path_async(). ⏰ Runtime: {(time.monotonic() - start_time):.2f} seconds")
+        print(f"[2/3] Completed get_chemoenzy_path_async(). Runtime: {(time.monotonic() - start_time):.2f} seconds")
 
     start_time = time.monotonic()
     if 'explored_rxns' not in results or 'explored_nodes' not in results or 'start_node' not in results:
-        parser.error("❌ Error: The results dictionary must contain 'explored_rxns', 'explored_nodes', and 'start_node' for build_graph_from_async")
+        parser.error("xxx Error: The results dictionary must contain 'explored_rxns', 'explored_nodes', and 'start_node' for build_graph_from_async")
     else: 
         results['graph'] = build_graph_from_async_wrapper(results['explored_rxns'], results['explored_nodes'], results['start_node'])
-        print(f"[3/3] build_graph_from_async(). ⏰ Runtime: {(time.monotonic() - start_time):.2f} seconds")
+        print(f"[3/3] build_graph_from_async(). Runtime: {(time.monotonic() - start_time):.2f} seconds")
     
     # Upload the results to Minio
-    # TODO: Get env vars for Minio
-    # upload_json_to_minio(results, bucket='aceretro', path=f"/{args.job_id}/out/output.json")
+    try:
+        upload_json_to_minio(results, bucket='aceretro', path=f"/{args.job_id}/out/output.pickle")
+        print(f"Uploaded results to minio: bucket=aceretro, path=/{args.job_id}/out/output.pickle")
+    except Exception as e: 
+        print(f"Failed to upload results to Minio with error: {e}")
 
 if __name__ == "__main__":
     main()
